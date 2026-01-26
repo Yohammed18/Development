338b2faea5668b09a0c5a7a1936f75258dddd1fc
Mohammed Allibalogun <atdev2024@gmail.com>
2026-01-25 22:22:55 -0600

    Save data to generate html

---
+++ message.js
@@ -0,0 +1,6 @@
+chatMessages = [
+  { message: "Hello chatbox!", sender: "user" },
+  { message: "Hello! How can I help you today?", sender: "bot" },
+  { message: "Can you tell me today's date?", sender: "user" },
+  { message: `Today is ${new Date().toDateString()}`, sender: "bot" }
+];
